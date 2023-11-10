3baf5b917131a7b7f5c3e35f092ab001394a4c10
Ini <arthurshur@gmail.com>
2023-11-09 22:00:24 -0800

    shell part 1

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - starts simple shell
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment variable
+ */
+int main(int ac, char **av, char **env)
+{
+	char *input, *token, *duplicate;
+	char **splits;
+	size_t size;
+	int status;
+	pid_t pid;
+
+	while (1)
+	{
+		input = prompt();
+		duplicate = strdup(input);
+		splits = strings(duplicate);
+
+		pid = fork();
+
+		if (pid == -1)
+			perror("Error");
+		else if (pid == 0)
+		{
+			if(execve(splits[0], splits, NULL) == -1)
+			{
+				perror(av[0]);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+
+	}
+
+	return (0);
+}
+
