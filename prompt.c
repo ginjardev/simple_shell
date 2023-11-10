2453814c899cc60e8a437696da16057164ed9bfb
Ini <arthurshur@gmail.com>
2023-11-09 22:11:40 -0800

    prompt

---
+++ prompt.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prompt - prompts user
+ *
+ * Return: string read from cli
+ */
+char *prompt(void)
+{
+	char *buffer;
+	size_t size;
+	int count = 1;
+
+	while (count == 1)
+	{
+		printf("%s", "0xShell$ ");
+		count = getline(&buffer, &size, stdin);
+
+		if (count == -1)
+		{
+			free(buffer);
+			break;
+		}
+	}
+	if (count > 1 && buffer[count - 1] == '\n')
+		buffer[count - 1] = '\0';
+
+	return (buffer);
+}
+
