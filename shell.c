0dd82cbf4e07edebcc16fe96915ca1137a4309fa
Ini <arthurshur@gmail.com>
2023-11-15 09:08:53 -0800

    shell

--- shell.c
+++ shell.c
@@ -4,40 +4,23 @@
  * main - starts simple shell
  * @ac: argument count
  * @av: argument vector
- * @env: environment variable
+ *
  * Return: 0 if successful
  */
-int main(__attribute__((unused)) int ac, char **av, char **env)
+int main(int ac, char **av)
 {
+	int check = 1;
 	char *input, *duplicate, *command;
-	char **splits;
-	int status;
-	pid_t pid;
+	(void) ac;
 
-	while (1)
+	while (check)
 	{
 		input = prompt();
 		duplicate = strdup(input);
-		splits = split_str(duplicate);
+		av = split_str(duplicate);
+		command = av[0];
 
-		pid = fork();
-
-		if (pid == -1)
-			perror("Error");
-		else if (pid == 0)
-		{
-			command = get_addy(splits[0]);
-
-			if (execve(command, splits, env) == -1)
-			{
-				perror(av[0]);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
-
+		check = exec_all(command, av);
 	}
 
 	return (0);
