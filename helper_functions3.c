cd1c5c8540a7afe88bc9e86c8ab8aab52090073c
Ini <arthurshur@gmail.com>
2023-11-15 09:06:50 -0800

    prompt & hfunc

--- helper_functions3.c
+++ helper_functions3.c
@@ -1,36 +1,48 @@
 #include "main.h"
 
 /**
- * _execute - executes user commands
+ * exec_command - executes commands
  * @command: user command
+ * @av: command line arguments
  *
- * Return: 0 on success
+ * Return: 1 on success
  */
-
-int exec_builtin(char *command)
+int exec_command(char *command, char **av)
 {
-	int (*perform)(char *command);
+	pid_t pid;
+	int status;
 
-	perform = get_builtin(command);
+	pid = fork();
 
-	if (perform != NULL)
-		return (perform(command));
+	if (pid == -1)
+		perror("Error");
+	else if (pid == 0)
+	{
+		command = get_addy(command);
+
+		if (execve(command, av, environ) == -1)
+			perror(av[0]);
+	}
 	else
-		return (1);
+		wait(&status);
+
+	return (1);
 }
 
 /**
- * exec_builtin - executes built-in commands
- * @command: user command
+ * get_builtin - executes built-in commands
+ * @str: function pointer argument
  *
- * Return: 0 on success
+ * Return: function pointer
  */
-int (*get_builtin(char *str))(char *command)
+int (*get_builtin(char *str))(char **av)
 {
 	int counter = 0;
 
 	Builtin arr[] = {
 		{"exit", exit_sh},
+		{"cd", cd_dir},
+		{"env", _env},
 		{NULL, NULL}
 	};
 
@@ -42,5 +54,92 @@ int (*get_builtin(char *str))(char *command)
 	}
 
 	return (arr[counter].func);
+}
+
+/**
+ * exec_all - executes user commands
+ * @command: user command
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int exec_all(char *command, char **av)
+{
+	int (*function)(char **av);
+
+	if (av[0] == NULL)
+		return (1);
+
+	function = get_builtin(av[0]);
+
+	if (function != NULL)
+		return (function(av));
+	else
+		return (exec_command(command, av));
+}
+
+/**
+ * _env - prints the evironment variables
+ *
+ * Return: 1 on success.
+ */
+int _env(void)
+{
+	int row, col;
+
+	for (row = 0; environ[row]; row++)
+	{
+
+		for (col = 0; environ[row][col]; col++)
+			;
+
+		write(STDOUT_FILENO, environ[row], col);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+
+	return (1);
+}
+
+/**
+ * cd_dir - changes current directory
+ * @av: arguments list
+ *
+ * Return: 1 on success
+ */
+int cd_dir(char **av)
+{
+	char buffer[4096];
+
+	if (av[1] == NULL)
+	{
+		/*Change directory to home directory*/
+		const char *home_dir = _getenv("HOME");
+
+		if (home_dir == NULL)
+		{
+			perror("HOME not set");
+			return (1);
+		}
+
+		if (chdir(home_dir) == 0)
+		{
+			if (getcwd(buffer, 4096) == NULL)
+				perror("Can't print dir");
+		}
+		else
+			perror("chdir error");
+	}
+	else
+	{
+		if (chdir(av[1]) == 0)
+		{
+			if (getcwd(buffer, 4096) == NULL)
+				perror("Can't print dir");
+		}
+		else
+			perror("chdir error");
+	}
+	return (1);
 }
 
--- prompt.c
+++ prompt.c
@@ -10,16 +10,16 @@ char *prompt(void)
 	char *buffer = NULL;
 	size_t size;
 	int count = 1;
+	char *prompt = "(^_*) ";
 
 	while (count == 1)
 	{
-		printf("%s", "0xShell$ ");
+		write(1, prompt, 6);
 		count = getline(&buffer, &size, stdin);
 
 		if (count == -1)
 		{
 			free(buffer);
-			break;
 		}
 	}
 	if (count > 1 && buffer[count - 1] == '\n')
