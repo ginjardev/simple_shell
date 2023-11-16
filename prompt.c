8fa936c1f3b460f80cc764fe153ca0197626accc
Ini <arthurshur@gmail.com>
2023-11-15 16:25:53 -0800

    shell loop non-interactive

--- prompt.c
+++ prompt.c
@@ -12,19 +12,15 @@ char *prompt(void)
 	int count = 1;
 	char *prompt = "(^_*) ";
 
-	while (count == 1)
+	write(1, prompt, 6);
+	count = getline(&buffer, &size, stdin);
+
+	if (count == -1 || count == 0)
 	{
-		write(1, prompt, 6);
-
 		free(buffer);
-
-		count = getline(&buffer, &size, stdin);
-
-		if (count == -1)
-		{
-			free(buffer);
-		}
+		return (NULL);
 	}
+
 	if (count > 1 && buffer[count - 1] == '\n')
 		buffer[count - 1] = '\0';
 
